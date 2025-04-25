0426d3b143f61076481e36e1c281ca5f5e102f9e
dex4367 <dexter436720@gmail.com>
2025-04-24 23:42:31 -0300

    feat: Adicionar página de teste de câmera

---
+++ src/routes.tsx
@@ -0,0 +1,52 @@
+import { Navigate, createBrowserRouter } from 'react-router-dom'
+import Layout from './components/Layout'
+import ProductForm from './pages/ProductForm'
+import ProductList from './pages/ProductList'
+import Reports from './pages/Reports'
+import CameraTest from './pages/CameraTest'
+import Dashboard from './pages/Dashboard'
+import MobileReceive from './pages/MobileReceive'
+import { Outlet } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout><Outlet /></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="/dashboard" replace />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      },
+      {
+        path: '/products/add',
+        element: <ProductForm />
+      },
+      {
+        path: '/products/edit/:id',
+        element: <ProductForm />
+      },
+      {
+        path: '/products',
+        element: <ProductList />
+      },
+      {
+        path: '/reports',
+        element: <Reports />
+      },
+      {
+        path: '/mobile-receive',
+        element: <MobileReceive />
+      },
+      {
+        path: '/camera-test',
+        element: <CameraTest />
+      }
+    ]
+  }
+])
+
+export default router 
